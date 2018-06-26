69a6f114b66936055e92745370bb7a37c9371ff4
icharle <icharle@live.com>
2018-06-26 17:34:38 +0800

    小程序码图片封装入口类方法

--- src/Wxtool.php
+++ src/Wxtool.php
@@ -11,5 +11,16 @@ namespace Icharle\Wxtool;
 
 class Wxtool
 {
+    /**
+     * @param $scene 场景值
+     * @param $page  页面值
+     * @return bool|string
+     */
+    public function GetQrcode($scene, $page)
+    {
+        $Qrcode = new Qrcode();
+        $imgpath = $Qrcode->GetCodeUnlimit($scene, $page);
+        return $imgpath;
+    }
 
 }
